0c1dbd5361922c2a52b9247f03fa1202e8d34615
Paula Hasstenteufel <paula@hasstenteufel.com>
2025-09-04 16:46:17 +0200

    Modificando o método para receber um array de dados.

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
 
 button.addEventListener('click', function () {
     // Assíncrona Async Chaining
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch('https://jsonplaceholder.typicode.com/posts')
     .then(function (response) {
         return response.json();
     })
-    .then(function (json) {
-        console.log(json);
+    .then(function (data) {
+        div.textContent = `Title: ${data[0].title}, Body: ${data[0].body}`
+        // let listaOrdenada = "<ol>";
+        
+        for (user in data) {
+            // "<li>" + `Title: ${usuario.title}` + "</li>";
+            // listaOrdenada += 
+        }
     })
 });
