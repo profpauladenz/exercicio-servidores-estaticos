68d75defdb2a5e2d9d8719775c668a2bf27a887c
Paula Hasstenteufel <paula@hasstenteufel.com>
2025-09-11 14:07:13 +0200

    Adicionando propriedades e construtor para as classe Post

--- scripts/post.js
+++ scripts/post.js
@@ -1,1 +1,16 @@
-class Post {}
+// Data Model (Modelo de Dados)
+class Post {
+    constructor(userId, id, title, body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+}
+
+// {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+// }
